ad413581b74b6e9a76a47e797e64f754d161f65d
MCGiako <giacometto.r.h2@gmail.com>
2021-10-04 10:20:02 +0000

    Otro Avance

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,11 +6,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-dark bg-dark mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1 text-warning">Star Wars</span>
 			</Link>
 			<div className="ml-auto">
 				<Dropdown>
-					<Dropdown.Toggle variant="success" id="dropdown-basic">
+					<Dropdown.Toggle variant="warning" id="dropdown-basic">
 						Dropdown Button
 					</Dropdown.Toggle>
 
